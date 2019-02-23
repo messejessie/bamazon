000c558a86e432cbd70d46426bc9bfc964cf87f8
messejessie <messejessie77@gmail.com>
2019-02-23 14:53:45 -0500

    bamazon lists working

--- bamazon.sql
+++ bamazon.sql
@@ -4,7 +4,7 @@ CREATE DATABASE products_DB;
 USE products_DB;
 
 CREATE TABLE inventory(
-  item_id INT NOT NULL AUTO_INCREMENT,
+  item_id INTEGER(10) NOT NULL AUTO_INCREMENT,
   item_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(45) NOT NULL,
   price DECIMAL(10,2) NULL,
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,8 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+let mysql = require("mysql");
+let inquirer = require("inquirer");
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -17,32 +17,80 @@ var connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
-  connection.end();
+  //connection.end();
   console.log("I works");
+  start()
 });
-
 function start() {
-  inquirer
-    .prompt({
-      name: "Departments",
-      type: "list",
-      message: "What Department would you like to took at?",
-      choices: ["", "", ""]
-    })
-    .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.postOrBid === "POST") {
-        postAuction();
-      }
-      else if(answer.postOrBid === "BID") {
-        bidAuction();
-      } else{
-        connection.end();
-      }
-    });
+    connection.query("SELECT * FROM inventory", function(err, results) {
+
+    if (err) throw err;
+    //console.log(results);
+    // once you have the items, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              var item_info = "";
+              item_info = results[i].item_name + " " + results[i].department_name + " " + "price =" + " " + results[i].price;
+              choiceArray.push(item_info);
+            }
+            return choiceArray;
+          },
+          message: "What Item would you like to purchase?"
+        },
+        {
+          name: "qty",
+          type: "input",
+          message: "How many would you like to purachase?"
+        },
+        {
+          type: "confirm",
+          message: "Are you sure you want to purchase?",
+          default: true
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].item_name === answer.choice) {
+            chosenItem = results[i];
+          }
+        }
+
+        // determine if bid was high enough
+        if (chosenItem.price< parseInt(answer.price)) {
+          // bid was high enough, so update db, let the user know, and start over
+          connection.query(
+            "UPDATE auctions SET ? WHERE ?",
+            [
+              {
+                highest_bid: answer.bid
+              },
+              {
+                id: chosenItem.id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("Bid placed successfully!");
+              start();
+            }
+          );
+        }
+        else {
+          // bid wasn't high enough, so apologize and start over
+          console.log("Your bid was too low. Try again...");
+          start();
+        }
+      });
+  });
 }
-
-
